6e2d8d7429d047d26e4238670bcf921a9ee2e330
Daniel Scott Smith <danielscottsmith@stanford.edu>
2022-11-19 14:33:08 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Replication package for 'Women, World Society, and the Expansion of Academic Knowledge in Education'',
+    author='Daniel Scott Smith',
+    license='MIT',
+)
